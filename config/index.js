236439a92788ad1d3fcd4f6210624483357e9f22
arthur-user <115193136+arthur-user@users.noreply.github.com>
2024-11-12 18:35:46 -0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
-const { Pool } = require('pg');
+const { Pool } = require('pg'); 
 
-// Use DATABASE_URL if it's available (Render will provide this in production)
-const connectionString = process.env.DATABASE_URL;
-
+// Change to development as necessary
 const isProduction = process.env.NODE_ENV === 'production';
+
+// Use DATABASE_URL if available (Render is going to automatically provide this in production)
+// else, manually construct the connection string for the purpose of local development
+const connectionString = isProduction ? process.env.DATABASE_URL : 
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 // Create a new Pool to manage database connections
 const db = new Pool({
